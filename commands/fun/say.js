57ce3824052cfa6c10a95ee21bea063568928a5d
Kinan <kinan.chahid2011@gmail.com>
2026-07-08 16:23:55 +0400

    Updated bot features

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -11,6 +11,16 @@ module.exports = {
         ),
 
     async execute(interaction) {
+        const hrRoleId = "1481953102654607451"; // HR role ID
+
+        // Permission check (same style as /purge)
+        if (!interaction.member.roles.cache.has(hrRoleId)) {
+            return interaction.reply({
+                content: "You do not have permission to use this command.",
+                flags: 64
+            });
+        }
+
         const message = interaction.options.getString("message");
 
         // Confirm privately
