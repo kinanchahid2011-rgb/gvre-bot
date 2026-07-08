e65a7d234cebe1899f47620bb2eb077b54c892ee
Kinan <kinan.chahid2011@gmail.com>
2026-07-08 16:14:52 +0400

    Add express web server for Render

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is alive!"));
+app.listen(3000, () => console.log("Web server running on port 3000"));
 const {
   Client,
   GatewayIntentBits,
