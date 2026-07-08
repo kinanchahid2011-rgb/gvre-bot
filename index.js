58d837e46f229507bc7c4468c9763de34d46838f
Kinan <kinan.chahid2011@gmail.com>
2026-07-08 18:34:09 +0400

    Added event loader for welcome system

--- index.js
+++ index.js
@@ -90,6 +90,23 @@ for (const folder of commandFolders) {
 }
 
 // -----------------------------------------------------
+// LOAD EVENTS
+// -----------------------------------------------------
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// -----------------------------------------------------
 // READY EVENT
 // -----------------------------------------------------
 client.once(Events.ClientReady, () => {
